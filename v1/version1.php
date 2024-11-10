b4dfcbfd231306392b2c788ab498af853f279b68
imaldonadom <ignacio.maldonado.m@gmail.com>
2024-11-10 00:07:40 -0300

    respaldar trabajo 1

--- v1/version1.php
+++ v1/version1.php
@@ -4,35 +4,35 @@ $_ubicacion = $_SERVER['HTTP_HOST'];
 $_metodo = $_SERVER['REQUEST_METHOD']; 
 $_path = $_SERVER['REQUEST_URI']; 
 $_partes = explode('/', $_path);
-$_version = $_ubicacion == 'localhost' ? $_partes[2] : null;
-$_mantenedor = $_ubicacion == 'localhost' ? $_partes[3] : null;
-$_parametros = [];
-$_parametros = $_ubicacion == 'localhost' ? $_partes[4] : null;
+$_version = $_ubicacion == 'localhost' && isset($_partes[2]) ? $_partes[2] : null;
+$_mantenedor = $_ubicacion == 'localhost' && isset($_partes[3]) ? $_partes[3] : null;
+$_parametros = isset($_partes[4]) ? $_partes[4] : null;
 
-if (strlen($_parametros)>0){
+if ($_parametros && strpos($_parametros, '?') !== false) {
     $_parametros = explode('?', $_parametros)[1];
     $_parametros = explode('&', $_parametros);
-}else{
+} else {
     $_parametros = [];
 }
 
-header("Access-Control-Allow-Origin: *"); // restriccion de acceso
-header("Access-Control-ALlow-Methods: GET, POST, PUT, PATH, DELETE");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE");
 header("Content-Type: application/json; charset=UTF-8");
 
-//Authorization Bearer
-$_header = null;
+// Authorization Bearer
 try {
-    $_header = isset(getallheaders()['Authorization']) ? getallheaders()['Authorization'] : null;
-    if($_header === null){
-        throw new Exception('No tiene autorizacion');
+    $_headers = getallheaders();
+    $_header = isset($_headers['Authorization']) ? $_headers['Authorization'] : null;
+    if ($_header === null) {
+        throw new Exception('No tiene autorización');
     }
 } catch (Exception $e) {
     http_response_code(401);
     echo json_encode(['error' => $e->getMessage()]);
+    exit;
 }
 
-//Tokens
+// Tokens
 $_token_get = 'Bearer get';
 $_token_get_evaluacion = 'Bearer ciisa';
 $_token_post = 'Bearer post';
